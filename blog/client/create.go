44c74f71bf72fb5d03b8d1d715e6ae999f122d00
Jhenifer <jhenifer.msantos@gmail.com>
2022-08-04 17:06:01 -0300

    Iniciando implementação do blog

---
+++ blog/client/create.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/jheniferms/go-grpc/blog/proto"
+)
+
+func createBlog(c pb.BlogServiceClient) string {
+	log.Println("createBlog was invoked")
+
+	blog := &pb.Blog{
+		AuthorId: "jheni",
+		Title:    "my first book",
+		Content:  "helloo",
+	}
+
+	res, err := c.CreateBlog(context.Background(), blog)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("Blog has been created: %s\n", res.Id)
+
+	return res.Id
+}
