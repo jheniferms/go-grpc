508a067c6d9b69eeaa55218403ec47c2c8505558
Jhenifer <jhenifer.msantos@gmail.com>
2022-08-04 17:31:05 -0300

    Implementado leitura

---
+++ blog/server/read.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	pb "github.com/jheniferms/go-grpc/blog/proto"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+func (s *Server) ReadBlog(ctx context.Context, in *pb.BlogId) (*pb.Blog, error) {
+	log.Printf("ReadBlog was invoked with:%v", in)
+
+	oid, err := primitive.ObjectIDFromHex(in.Id)
+
+	if err != nil {
+		return nil, status.Errorf(
+			codes.InvalidArgument,
+			"Cannot parse ID",
+		)
+	}
+
+	filter := bson.M{"_id": oid}
+
+	res := collection.FindOne(ctx, filter)
+
+	data := &BlogItem{}
+
+	if err := res.Decode(data); err != nil {
+		return nil, status.Errorf(
+			codes.NotFound,
+			fmt.Sprintf("Cannot find blog with specified ID: %v", err),
+		)
+	}
+
+	return documentToBlog(data), nil
+}
